bf9da6ec766c39313acedcfb7a25422a436ca753
Chris Bateman <chrisb808@gmail.com>
2014-12-16 14:19:23 +0000

    demos

---
+++ demos/import-demo/import.js
@@ -0,0 +1,4 @@
+console.log('This is a script in the import');
+
+// this script executes in the importing page's scope, as usual
+var importScript = 'this is now a global variable on the importing page';
